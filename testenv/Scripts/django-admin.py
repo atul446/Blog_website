efabc19d8cb2b80e144cb01aea64899932fae341
atul446 <atuljai2000@gmail.com>
2021-08-23 09:47:15 +0530

    uploading it again

--- testenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\atul jain\desktop\web dev\django practice\testenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
